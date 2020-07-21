2c76afb061f522c8fc583a3e5d82341a2069dd43
Ooalh <zhangyifansugar@gmail.com>
2020-07-21 08:30:37 +0800

    frontend

--- examples.js
+++ examples.js
@@ -1,29 +1,22 @@
-/*
-const control1 = controlWidget('a')
-document.querySelector('body').append(control1)
-const text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-const note1 = noteWidget(text, 'a', 'width: 80%; word-wrap: break-word;')
-document.querySelector('body').append(note1)
-const note2 = noteWidget(text, 'a', 'width: 70%; word-wrap: break-word;')
-document.querySelector('body').append(note2)
-const control2 = controlWidget('b','opacity: 0.5; background: red;')
-document.querySelector('body').append(control2)
-const note3 = noteWidget(text, 'b', 'width: 100%; word-wrap: break-word;')
-document.querySelector('body').append(note3)
-*/
 const pair1 = new EasyNotePair('a')
-const pair2 = new EasyNotePair('b', 'opacity: 0.5; background: aqua;', true)
+const pair2 = new EasyNotePair('b', 'background: rgba(134, 204, 236, 0.445);', true)
 const control1 = pair1.controller
 const control2 = pair2.controller
+
 const text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
 
-const note1 = pair1.createNoteWidget(text, 'width: 80%; word-wrap: break-word;')
-const note2 = pair1.createNoteWidget(text, 'width: 60%; word-wrap: break-word;')
-const note3 = pair2.createNoteWidget(text)
-document.querySelector('body').append(control1)
-document.querySelector('body').append(note1)
-document.querySelector('body').append(note2)
-document.querySelector('body').append(control2)
-document.querySelector('body').append(note3)
+const note1 = pair1.createNoteWidget(text)
+const note2 = pair1.createNoteWidget(text)
+const note3 = pair2.createNoteWidget(text, 'color: rgba(62, 100, 117, 0.445)')
 
-console.log(pair1.getHighlights())
+const article1 = document.getElementById('article1')
+const article2 = document.getElementById('article2')
+const section1 = document.getElementById('section1')
+const section2 = document.getElementById('section2')
+article1.insertBefore(control1, section1)
+article1.insertBefore(note1, section2)
+article1.append(note2)
+article2.append(control2)
+article2.append(note3)
+
+//console.log(pair1.getHighlights())
